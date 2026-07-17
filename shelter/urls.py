bacad113d0df00d3373b1225b2e6f27e749f291e
elenabatsog <e.batsogianni@acg.edu>
2026-07-17 22:07:58 +0300

    Added my_suggestions path

--- shelter/urls.py
+++ shelter/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("favourites/", views.favourites, name="favourites"),
     path("animals/<int:pet_id>/favourite/", views.toggle_favourite, name="toggle_favourite"),
     path("suggest/", views.suggest_animal, name="suggest_animal"),
+    path("my-suggestions/", views.my_suggestions, name="my_suggestions"),
     path("manage/", views.manage_animals, name="manage_animals"),
     path("animals/<int:pet_id>/", views.pet_details, name="pet_details"),
 ]
