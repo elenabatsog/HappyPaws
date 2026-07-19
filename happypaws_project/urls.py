349b2d1c1cdf297300a52d698e34d4a713a2e24c
elenabatsog <e.batsogianni@acg.edu>
2026-07-19 20:19:50 +0300

    commit

--- happypaws_project/urls.py
+++ happypaws_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,5 +8,11 @@ urlpatterns = [
     path("shelter/", include("shelter.urls")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
 
 
