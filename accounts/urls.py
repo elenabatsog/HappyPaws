dbe5a9bcd078dcfc1765111aecbe6a6c95e78c31
elenabatsog <e.batsogianni@acg.edu>
2026-07-19 09:39:23 +0300

    Added new paths for navigation bar

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", views.welcome, name="welcome"),
+    path("home/", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("register/", views.register_request, name="register"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("logout/", views.logout_request, name="logout"),
