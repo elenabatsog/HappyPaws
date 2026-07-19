23dc24f70cc56f0cacfa7e8689f7e3c74bc39dba
elenabatsog <e.batsogianni@acg.edu>
2026-07-19 20:14:07 +0300

    Edit Profile Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -112,3 +112,34 @@ class NewUserForm(UserCreationForm):
 
         return user 
     
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+        ]
+
+        widgets = {
+            "first_name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter your first name",
+                }
+            ),
+
+            "last_name": forms.TextInput(
+                attrs={
+                    "class": "form_control",
+                    "placeholder": "Enter your last name",
+                }
+            ),
+
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form_control",
+                    "placeholder": "Enter your email ",
+                }
+            ),
+        }
